000c3c3f6c740ba3bda179ef51b112e283970594
Jacob <jmarkus1111@gmail.com>
2024-08-05 15:45:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+    name='resmorph',
+    version='0.1.0',    
+    description='High redshift galaxy morphology classification tool',
+    url='https://github.com/jmarkus1111/resmorph',
+    author='Jacob Markus',
+    author_email='marku120@umn.edu',
+    license='',
+    packages=['resmorph'],
+    install_requires=[                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+    ],
+)
